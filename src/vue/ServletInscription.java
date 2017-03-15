c48082609bdebb4cc404b5e8dcb2ee788bb069fc
tonyskinsy <noubijunior@gmail.com>
2017-03-15 16:05:02 +0100

    petites modifications

--- src/vue/ServletInscription.java
+++ src/vue/ServletInscription.java
@@ -1,41 +1,30 @@
 package vue;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ServletInscription
- */
-@WebServlet("/ServletInscription")
+import beans.Person;
+import dao.PersonDao;
+import dao.UsineDao;
+import forms.FormInscription;
+
+@SuppressWarnings("serial")
 public class ServletInscription extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public ServletInscription() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
+	public static final String CONF_DAO_FACTORY = "usinedao";
+	public static final String ATT_USER = "utilisateur";
+	public static final String ATT_FORM = "form";
 	public static final String VUE = "/WEB-INF/inscription.jsp";
-	public static final String CHAMP_EMAIL = "email";
-	public static final String CHAMP_PASS = "motdepasse";
-	public static final String CHAMP_CONF = "confirmation";
-	public static final String CHAMP_NOM = "nom";
-	public static final String ATT_ERREURS = "erreurs";
-	public static final String ATT_RESULTAT = "resultat";
+
+	private PersonDao utilisateurDao;
+
+	public void init() throws ServletException {
+		/* Récupération d'une instance de notre DAO Utilisateur */
+		this.utilisateurDao = ((UsineDao) getServletContext().getAttribute(CONF_DAO_FACTORY)).getUtilisateurDao();
+	}
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		/* Affichage de la page d'inscription */
@@ -43,87 +32,16 @@ public class ServletInscription extends HttpServlet {
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String resultat;
-		Map<String, String> erreurs = new HashMap<String, String>();
+		/* Préparation de l'objet formulaire */
+		FormInscription form = new FormInscription(utilisateurDao);
 
-		/* Récupération des champs du formulaire. */
-		String email = request.getParameter(CHAMP_EMAIL);
-		String motDePasse = request.getParameter(CHAMP_PASS);
-		String confirmation = request.getParameter(CHAMP_CONF);
-		String nom = request.getParameter(CHAMP_NOM);
+		/* Traitement de la requête et récupération du bean en résultant */
+		Person utilisateur = form.inscrireUtilisateur(request);
 
-		/* Validation du champ email. */
-		try {
-			validationEmail(email);
-		} catch (Exception e) {
-			erreurs.put(CHAMP_EMAIL, e.getMessage());
-		}
+		/* Stockage du formulaire et du bean dans l'objet request */
+		request.setAttribute(ATT_FORM, form);
+		request.setAttribute(ATT_USER, utilisateur);
 
-		/* Validation des champs mot de passe et confirmation. */
-		try {
-			validationMotsDePasse(motDePasse, confirmation);
-		} catch (Exception e) {
-			erreurs.put(CHAMP_PASS, e.getMessage());
-		}
-
-		/* Validation du champ nom. */
-		try {
-			validationNom(nom);
-		} catch (Exception e) {
-			erreurs.put(CHAMP_NOM, e.getMessage());
-		}
-
-		/* Initialisation du résultat global de la validation. */
-		if (erreurs.isEmpty()) {
-			resultat = "Succès de l'inscription.";
-		} else {
-			resultat = "Échec de l'inscription.";
-		}
-
-		/* Stockage du résultat et des messages d'erreur dans l'objet request */
-		request.setAttribute(ATT_ERREURS, erreurs);
-		request.setAttribute(ATT_RESULTAT, resultat);
-
-		/* Transmission de la paire d'objets request/response à notre JSP */
 		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
 	}
-
-	/**
-	 * Valide l'adresse mail saisie.
-	 */
-	private void validationEmail(String email) throws Exception {
-		if (email != null && email.trim().length() != 0) {
-			if (!email.matches("([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)")) {
-				throw new Exception("Merci de saisir une adresse mail valide.");
-			}
-		} else {
-			throw new Exception("Merci de saisir une adresse mail.");
-		}
-	}
-
-	/**
-	 * Valide les mots de passe saisis.
-	 */
-	private void validationMotsDePasse(String motDePasse, String confirmation) throws Exception {
-		if (motDePasse != null && motDePasse.trim().length() != 0 && confirmation != null
-				&& confirmation.trim().length() != 0) {
-			if (!motDePasse.equals(confirmation)) {
-				throw new Exception("Les mots de passe entrés sont différents, merci de les saisir à nouveau.");
-			} else if (motDePasse.trim().length() < 3) {
-				throw new Exception("Les mots de passe doivent contenir au moins 3 caractères.");
-			}
-		} else {
-			throw new Exception("Merci de saisir et confirmer votre mot de passe.");
-		}
-	}
-
-	/**
-	 * Valide le nom d'utilisateur saisi.
-	 */
-	private void validationNom(String nom) throws Exception {
-		if (nom != null && nom.trim().length() < 3) {
-			throw new Exception("Le nom d'utilisateur doit contenir au moins 3 caractères.");
-		}
-	}
-
 }
