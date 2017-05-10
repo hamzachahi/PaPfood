e4846a897bb9591413194f175c7c9e432d5200c2
yferchichi <ferchichi.youssef@yahoo.fr>
2017-05-10 23:46:05 +0200

    déconnexion

--- WebContent/WEB-INF/header.jsp
+++ WebContent/WEB-INF/header.jsp
@@ -31,8 +31,8 @@
 			</div>
 			<div class="col-sm-2 number animated wow fadeInRight"
 				data-wow-delay=".5s">
-				<span><a href="ServletProfile"><i class="glyphicon glyphicon-phone"></i>Modifier Mon compte</a></span>
-				<p>Call Junior</p>
+				<span><a href="ServletProfile"><i
+						class="glyphicon glyphicon-phone"></i>Modifier Mon compte</a></span>
 			</div>
 			<div class="col-sm-2 search animated wow fadeInRight"
 				data-wow-delay=".5s">
@@ -62,66 +62,23 @@
 					<!-- Collect the nav links, forms, and other content for toggling -->
 					<div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
 						<ul class="nav navbar-nav nav_1">
-							<li><a href="index.html">Home</a></li>
-							<li class="dropdown mega-dropdown active"><a href="#"
-								class="dropdown-toggle" data-toggle="dropdown">Women<span
-									class="caret"></span></a>
-								<div class="dropdown-menu mega-dropdown-menu">
-									<div class="container-fluid">
-										<!-- Tab panes -->
-										<div class="tab-content">
-											<div class="tab-pane active" id="men">
-												<ul class="nav-list list-inline">
-													<li><a href="women.html"><img src="images/t7.jpg"
-															class="img-responsive" alt="" /></a></li>
-													<li><a href="women.html"><img src="images/t8.jpg"
-															class="img-responsive" alt="" /></a></li>
-													<li><a href="women.html"><img src="images/t9.jpg"
-															class="img-responsive" alt="" /></a></li>
-													<li><a href="women.html"><img src="images/t11.jpg"
-															class="img-responsive" alt="" /></a></li>
-													<li><a href="women.html"><img src="images/t1.jpg"
-															class="img-responsive" alt="" /></a></li>
-													<li><a href="women.html"><img src="images/t12.jpg"
-															class="img-responsive" alt="" /></a></li>
-												</ul>
-											</div>
-										</div>
-									</div>
-									<!-- Nav tabs -->
+							<li><a href="${pageContext.request.contextPath}/accueil">Accueil</a></li>
+							<li><a href="${pageContext.request.contextPath}/acheter">Acheter
+									un produit</a></li>
+							<li><a href="${pageContext.request.contextPath}/proposer">Proposer
+									un produit</a></li>
+							<c:if test="${empty sessionUtilisateur}">
+								<li><a href="${pageContext.request.contextPath}/connexion">Se
+										connecter</a></li>
+							</c:if>
 
-								</div></li>
-							<li class="dropdown mega-dropdown active"><a href="#"
-								class="dropdown-toggle" data-toggle="dropdown">Catégories<span
-									class="caret"></span></a>
-								<div class="dropdown-menu mega-dropdown-menu">
-									<div class="container-fluid">
-										<!-- Tab panes -->
-										<div class="tab-content">
-											<div class="tab-pane active" id="men">
-												<ul class="nav-list list-inline">
-													<li><a href="men.html"><img src="images/pizza.png"
-															class="img-responsive" alt="" /></a> Italien</li>
-													<li><a href="men.html"><img src="images/nem.png"
-															class="img-responsive" alt="" /></a>Asiatique</li>
-													<li><a href="men.html"><img
-															src="images/salade.png" class="img-responsive" alt="" /></a>Salade</li>
-													<li><a href="men.html"><img
-															src="images/dessert.png" class="img-responsive" alt="" /></a>Dessert</li>
-													<!--  <li><a href="men.html"><img src="images/t5.jpg" class="img-responsive" alt=""/></a></li>
-												<li><a href="men.html"><img src="images/t6.jpg" class="img-responsive" alt=""/></a></li>
-											-->
-												</ul>
-											</div>
+							<c:if test="${not empty sessionUtilisateur}">
+								<li><a href="\ServletConnect?action=logOut">Se
+										déconnecter</a></li>
+							</c:if>
 
-										</div>
-									</div>
-									<!-- Nav tabs -->
-
-								</div></li>
-							<li><a href="products.html">Products</a></li>
-							<li><a href="account.html">Se connecter</a></li>
-							<li class="last"><a href="contact.html">Contact</a></li>
+							<li class="last"><a
+								href="${pageContext.request.contextPath}/contact">Contact</a></li>
 						</ul>
 					</div>
 					<!-- /.navbar-collapse -->
--- src/vue/ServletConnect.java
+++ src/vue/ServletConnect.java
@@ -18,56 +18,65 @@ import forms.FormulaireConnexion;
  */
 @WebServlet("/Connect")
 public class ServletConnect extends HttpServlet {
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -823867703158698451L;
-	public static final String ATT_USER         = "utilisateur";
-    public static final String ATT_FORM         = "form";
-    public static final String ATT_SESSION_USER = "sessionUtilisateur";
-    public static final String VUE              = "/WEB-INF/connexion.jsp";
-    private PersonDao utilisateurDao ;
-    
-    
+	public static final String ATT_USER = "utilisateur";
+	public static final String ATT_FORM = "form";
+	public static final String ATT_SESSION_USER = "sessionUtilisateur";
+	public static final String VUE = "/WEB-INF/connexion.jsp";
+	public boolean isConnected = true;
+	private PersonDao utilisateurDao;
 
-    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
-        /* Affichage de la page de connexion */
-        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
-    }
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/* Affichage de la page de connexion */
+		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+	}
 
-    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
-		this.utilisateurDao = new DaoPersonImpl(new UsineDao("jdbc:oracle:thin:@localhost:1521:orcl", "papfood", "yummyshop"));
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.utilisateurDao = new DaoPersonImpl(
+				new UsineDao("jdbc:oracle:thin:@localhost:1521:orcl", "papfood", "yummyshop"));
 
-    	/* Préparation de l'objet formulaire */
-        FormulaireConnexion form = new FormulaireConnexion(utilisateurDao);
+		/* Préparation de l'objet formulaire */
+		FormulaireConnexion form = new FormulaireConnexion(utilisateurDao);
 
-        /* Traitement de la requête et récupération du bean en résultant */
-        Person utilisateur = null;
+		/* Traitement de la requête et récupération du bean en résultant */
+		Person utilisateur = null;
 		try {
-			utilisateur = form.connecterUtilisateur( request );
+			utilisateur = form.connecterUtilisateur(request);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
 
-        /* Récupération de la session depuis la requête */
-        HttpSession session = request.getSession();
+		/* Récupération de la session depuis la requête */
+		HttpSession session = request.getSession();
 
-        /**
-         * Si aucune erreur de validation n'a eu lieu, alors ajout du bean
-         * Utilisateur à la session, sinon suppression du bean de la session.
-         */
-        if ( form.getErreurs().isEmpty() ) {
-            session.setAttribute( ATT_SESSION_USER, utilisateur );
-        } else {
-            session.setAttribute( ATT_SESSION_USER, null );
-        }
+		String action = request.getParameter("action");
 
-        /* Stockage du formulaire et du bean dans l'objet request */
-        request.setAttribute( ATT_FORM, form );
-        request.setAttribute( ATT_USER, utilisateur );
+		/**
+		 * Si aucune erreur de validation n'a eu lieu, alors ajout du bean
+		 * Utilisateur à la session, sinon suppression du bean de la session.
+		 */
+		if (form.getErreurs().isEmpty()) {
+			session.setAttribute(ATT_SESSION_USER, utilisateur);
+			session.setAttribute("loggedIn", isConnected);
+		} else {
+			session.setAttribute(ATT_SESSION_USER, null);
+		}
 
-        this.getServletContext().getRequestDispatcher( "/WEB-INF/accueil.jsp" ).forward( request, response );
-    }
+		/* Stockage du formulaire et du bean dans l'objet request */
+		request.setAttribute(ATT_FORM, form);
+		request.setAttribute(ATT_USER, utilisateur);
+
+		this.getServletContext().getRequestDispatcher("/WEB-INF/accueil.jsp").forward(request, response);
+
+		if (action != null) {
+			if (action.equals("logOut")) {
+				session.invalidate();
+				// response.sendRedirect();
+			}
+		}
+	}
 }
